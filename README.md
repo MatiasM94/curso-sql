6a881fb333b4a31c1391f889ebf66139864bef4a
Matias Monzon <matiasmonzon_2004@hotmail.com>
2023-10-24 20:33:47 -0300

    agregue los archivos csv

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-## Este Repositorio incluira los archivos .sql de mi proyecto creado para el curso sql dictado por coderhouse.
+# Este Repositorio incluira los archivos .sql de mi proyecto creado para el curso sql dictado por coderhouse.
+
+## Inserción de datos con importación de archivos .csv
+
+Para una correcta inserción de datos hay que seguir el siguiente orden(los archivos se encuentran en la carpeta csv).
+
+### 1. Importar en la tabla parients el archivo **parients.csv**
+
+> Revisar que la Dest column **number_health_insurance** tenga correctamente su dato(en ocasiones phone_number es default en esta columna).
+
+### 2. Importar en la tabla medical_history el archivo **medical_history.csv**
+
+### 3. Importar en la tabla medical_speciality el archivo **medical_history.csv**
+
+### 4. Importar en la tabla medics el archivo **medics.csv**
+
+### 5. Importar en la tabla medical_shift el archivo **medical_shift.csv**
+
+## Ejemplo de consultas de datos
+
+Una vez realizado las importaciones, puede realizar consultas como la siguiente:
+
+```
+SELECT medical_shift.shift_date, patients.first_name AS patient, medics.first_name AS medic
+FROM medical_shift
+INNER JOIN patients ON medical_shift.patient_id = patients.id
+INNER JOIN medics ON medical_shift.medic_id = medics.id;
+```
--- medical_clinic.sql
+++ medical_clinic.sql
@@ -2,29 +2,36 @@ CREATE SCHEMA medical_clinic;
 USE medical_clinic;
 
 CREATE TABLE patients(
-	id VARCHAR(36) NOT NULL PRIMARY KEY,
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     document_type VARCHAR(10) NOT NULL,
     document_number INT NOT NULL UNIQUE,
     phone_number INT NOT NULL,
     number_healt_insurance INT UNIQUE,
+    province varchar(100),
+    city varchar(100),
     address_line varchar(100),
     cp varchar(20)
     );
     
 CREATE TABLE medical_history(
-	id VARCHAR(36) NOT NULL PRIMARY KEY,
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     medical_consultations VARCHAR(200),
     medical_treatment VARCHAR(200),
     emergencies VARCHAR(200),
-    medical_record VARCHAR(200),
-    patient_id VARCHAR(36) NOT NULL,
+    patient_id INT NOT NULL,
     FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
 
+CREATE TABLE medical_speciality(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    medical_area VARCHAR(200) NOT NULL,
+    speciality VARCHAR(200) NOT NULL
+);
+
 CREATE TABLE medics(
-	id VARCHAR(36) NOT NULL PRIMARY KEY,
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     document_type VARCHAR(10) NOT NULL,
@@ -32,24 +39,35 @@ CREATE TABLE medics(
     phone_number INT NOT NULL,
     address_line varchar(100),
     cp varchar(20),
-    medical_license INT NOT NULL UNIQUE
+    medical_license INT NOT NULL UNIQUE,
+    speciality_id INT NOT NULL,
+    FOREIGN KEY (speciality_id) REFERENCES medical_speciality(id)
 );
 
 CREATE TABLE medical_shift(
-	id VARCHAR(36) NOT NULL PRIMARY KEY,
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     shift_date DATE NOT NULL,
     place VARCHAR(200) NOT NULL,
     reason VARCHAR(200) NOT NULL,
-    patient_id VARCHAR(36) NOT NULL,
-    medic_id VARCHAR(36) NOT NULL,
+    patient_id INT NOT NULL,
+    medic_id INT NOT NULL,
     FOREIGN KEY (patient_id) REFERENCES patients(id),
     FOREIGN KEY (medic_id) REFERENCES medics(id)
 );
 
-CREATE TABLE medical_speciality(
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    medical_area VARCHAR(200) NOT NULL,
-    speciality VARCHAR(200) NOT NULL,
-    medic_id VARCHAR(36) NOT NULL,
-    FOREIGN KEY (medic_id) REFERENCES medics(id)
-);
+SELECT * FROM patients;
+SELECT * FROM medical_history;
+SELECT * FROM medical_speciality;
+
+SELECT *
+FROM medical_history
+INNER JOIN patients ON medical_history.patient_id = patients.id;
+
+SELECT * FROM medics;
+
+SELECT medical_shift.shift_date, patients.first_name AS patient, medics.first_name AS medic FROM medical_shift
+INNER JOIN patients ON medical_shift.patient_id = patients.id
+INNER JOIN medics ON medical_shift.medic_id = medics.id;
+
+SELECT * FROM medics
+INNER JOIN medical_speciality ON medics.speciality_id = medical_speciality.id;
